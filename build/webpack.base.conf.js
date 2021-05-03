6a28d761e62009ad39331cd7cebda9547e0f9cbe
kevin <kevin@kevinlan.top>
2021-05-03 23:58:32 +0800

    update webpack

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -40,6 +40,13 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+    },
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false
     }
   },
   module: {
@@ -89,16 +96,4 @@ module.exports = {
       }
     ]
   },
-  node: {
-    // prevent webpack from injecting useless setImmediate polyfill because Vue
-    // source contains it (although only uses it if it's native).
-    setImmediate: false,
-    // prevent webpack from injecting mocks to Node native modules
-    // that does not make sense for the client
-    dgram: 'empty',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty'
-  }
 }
